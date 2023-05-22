97987362020305891dce5ebc612223707199c0e9
Rijon <rijon.r@yahoo.com>
2023-05-22 09:41:11 +0600

    Update the job section

--- components/About/WorkPlace/WorkPlace.jsx
+++ components/About/WorkPlace/WorkPlace.jsx
@@ -1,23 +1,47 @@
-import React from "react";
+import { useState } from "react";
 
 const ExampleComponent = () => {
+  const [selectedCompany, setSelectedCompany] = useState(0);
+
+  const handleCompanySelect = (key) => {
+    setSelectedCompany(key);
+  };
+
+  const companyList = [
+    {
+      name: "Jatri",
+      key: 0,
+    },
+
+    {
+      name: "Shellbeehaken",
+      key: 1,
+    },
+
+    {
+      name: "MethodMelody",
+      key: 2,
+    },
+  ];
   return (
     <div className="w-11/12 sm:w-full md:w-10/12 mx-auto xl:min-h-20">
       <div className="w-full flex flex-col sm:flex-row">
         <div className="flex">
           <div className="flex flex-row sm:flex-col mb-8 sm:mb-0">
-            <h1 className="py-2 sm:py-3 px-6 sm:px-10 cursor-pointer hover:bg-secondary-30 font-bold md:font-semibold text-sm md:text-xl whitespace-nowrap mb-px transition-colors duration-300 bg-secondary-30 text-primary">
-              Welldev LTD
-            </h1>
-            <h1 className="py-2 sm:py-3 px-6 sm:px-10 cursor-pointer hover:bg-secondary-30 font-bold md:font-semibold text-sm md:text-xl whitespace-nowrap mb-px transition-colors duration-300 text-gray">
-              Xbit Studio
-            </h1>
+            {companyList.map((company) => (
+              <h1
+                onClick={() => handleCompanySelect(company.key)}
+                className="py-2 sm:py-3 px-6 sm:px-10 cursor-pointer hover:bg-[#4C6796] font-bold md:font-semibold text-sm md:text-xl whitespace-nowrap mb-px transition-colors duration-300 [#4C6796] text-[#D0DCF2]"
+              >
+                {company.name}
+              </h1>
+            ))}
           </div>
-          <div className="hidden sm:block relative bg-blue-200 w-0.5 h-full">
+          <div className="hidden sm:block relative bg-[#D0DCF2] w-0.5 h-full">
             <div
-              className="absolute top-0 left-0 w-full bg-black transform transition-transform duration-300 ease"
+              className="absolute top-0 left-0 w-full bg-[#4C6796] transform transition-transform duration-300 ease"
               style={{
-                transform: "translateY(calc(0% + 0px))",
+                transform: `translateY(calc(${selectedCompany * 100}% + 0px))`,
                 height: "52px",
               }}
             ></div>
