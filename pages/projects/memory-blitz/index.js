cdba7650d51272882fff0c92c8a661a889629217
Rijon <rijon.stack@gmail.com>
2025-01-14 12:57:18 +0600

    Final Version 2

--- pages/projects/memory-blitz/index.js
+++ pages/projects/memory-blitz/index.js
@@ -11,12 +11,28 @@ const MemoryGrid = () => {
   const [isUserTurn, setIsUserTurn] = useState(false);
   const [message, setMessage] = useState("");
   const [timeLeft, setTimeLeft] = useState(0);
+  const [difficulty, setDifficulty] = useState(2);
+  const [windowWidth, setWindowWidth] = useState(0);
 
   const highlightDuration = 1000;
   const userTimeLimit = 3000;
-  const numBoxesToHighlight = 1;
 
   const userSelectionRef = useRef(userSelection);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      setWindowWidth(window.innerWidth);
+      const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+      };
+      window.addEventListener("resize", handleResize);
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }
+  }, []);
+
+  const maxColumns = windowWidth < 640 ? 6 : 10;
 
   useEffect(() => {
     userSelectionRef.current = userSelection;
@@ -84,7 +100,7 @@ const MemoryGrid = () => {
     let initialGrid = generateGrid(gridSize.rows, gridSize.cols);
     let { updatedGrid, highlightedBoxes } = highlightBoxes(
       initialGrid,
-      numBoxesToHighlight
+      difficulty
     );
 
     setGrid(updatedGrid);
@@ -132,35 +148,58 @@ const MemoryGrid = () => {
       <h1 className="text-3xl font-bold text-orange-500 mb-6 text-center">
         Memory Blitz
       </h1>
-      <div className="flex flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-4 mb-6">
-        <div>
-          <label className="text-black-600 mr-2">Rows:</label>
-          <input
-            type="number"
-            value={gridSize.rows}
-            onChange={(e) =>
-              setGridSize({ ...gridSize, rows: parseInt(e.target.value) || 1 })
-            }
-            className="border p-2 rounded focus:ring-2 focus:ring-orange-500 focus:outline-none"
-            min="1"
-          />
+      <div className="flex flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-6 mb-8">
+        <div className="flex flex-col items-start">
+          <label className="text-lg font-semibold text-gray-700 mb-2">
+            Rows x Cols:
+          </label>
+          <select
+            value={`${gridSize.rows}x${gridSize.cols}`}
+            onChange={(e) => {
+              const [rows, cols] = e.target.value.split("x").map(Number);
+              setGridSize({ rows, cols });
+              if (difficulty > rows * cols) {
+                setDifficulty(rows * cols);
+              }
+            }}
+            className="w-[200px] border-2 border-gray-300 p-3 rounded-lg shadow-sm focus:ring-2 focus:ring-orange-500 focus:outline-none transition-all ease-in-out duration-200 hover:border-gray-500"
+          >
+            {Array.from({ length: maxColumns }, (_, i) => (
+              <option key={i} value={`${i + 1}x${i + 1}`}>
+                {i + 1} x {i + 1}
+              </option>
+            ))}
+          </select>
         </div>
-        <div>
-          <label className="text-black-600 mr-2">Cols:</label>
-          <input
-            type="number"
-            value={gridSize.cols}
+
+        <div className="flex flex-col items-start">
+          <label className="text-lg font-semibold text-gray-700 mb-2">
+            Set Difficulty:
+          </label>
+          <select
+            value={difficulty}
             onChange={(e) =>
-              setGridSize({ ...gridSize, cols: parseInt(e.target.value) || 1 })
+              setDifficulty(
+                Math.min(
+                  parseInt(e.target.value) || 2,
+                  gridSize.rows * gridSize.cols
+                )
+              )
             }
-            className="border p-2 rounded focus:ring-2 focus:ring-orange-500 focus:outline-none"
-            min="1"
-          />
+            className="w-[200px] border-2 border-gray-300 p-3 rounded-lg shadow-sm focus:ring-2 focus:ring-orange-500 focus:outline-none transition-all ease-in-out duration-200 hover:border-gray-500"
+          >
+            {Array.from({ length: gridSize.rows * gridSize.cols }, (_, i) => (
+              <option key={i} value={i + 1}>
+                {i + 1}
+              </option>
+            ))}
+          </select>
         </div>
       </div>
+
       <button
         onClick={startGame}
-        className="bg-orange-500 text-white px-6 py-2 rounded shadow-lg hover:bg-orange-600 transition duration-300"
+        className="bg-orange-500 text-white-300 px-6 py-2 rounded shadow-lg hover:bg-orange-600 transition duration-300"
       >
         Start Game
       </button>
@@ -171,16 +210,19 @@ const MemoryGrid = () => {
       )}
       {message && (
         <div
-          className={`mt-6 px-8 py-4 rounded-lg shadow-lg text-2xl font-semibold ${message === "You Win!"
+          className={`mt-6 px-8 py-4 rounded-lg shadow-lg text-2xl font-semibold text-center ${
+            message === "You Win!"
               ? "bg-gradient-to-r from-green-400 via-green-500 to-green-600 text-white animate-pulse"
               : "bg-gradient-to-r from-red-400 via-red-500 to-red-600 text-white animate-bounce"
-            }`}
+          }`}
         >
-          {message === "You Win!" ? "🎉 Congratulations, You Win! 🎉" : "💔 Oh no, You Lose! Try Again! 💔"}
+          {message === "You Win!"
+            ? "🎉 Congratulations, You Win! 🎉"
+            : "💔 Oh no, You Lose! Try Again! 💔"}
         </div>
       )}
       <div
-        className="grid gap-2 mt-6 w-full max-w-xl"
+        className="grid gap-2 mt-6 max-w-xl"
         style={{
           gridTemplateRows: `repeat(${gridSize.rows}, minmax(40px, 1fr))`,
           gridTemplateColumns: `repeat(${gridSize.cols}, minmax(40px, 1fr))`,
@@ -191,13 +233,14 @@ const MemoryGrid = () => {
             <div
               key={box.id}
               onClick={() => handleBoxClick(box.id)}
-              className={`flex justify-center items-center rounded cursor-pointer border ${box.isHighlighted
-                  ? "bg-orange-500 shadow-orange-md"
+              className={`flex justify-center items-center rounded cursor-pointer border ${
+                box.isHighlighted
+                  ? "bg-orange-500 shadow-orange-md border-gray-100"
                   : userSelection.includes(box.id)
-                    ? "bg-green-500"
-                    : "bg-gray-100 border-gray-400"
-                } hover:shadow-lg`}
-              style={{ minWidth: 40, minHeight: 40 }}
+                  ? "bg-green-500 border-gray-100"
+                  : "bg-gray-100 border-gray-400"
+              } hover:shadow-lg`}
+              style={{ maxWidth: 40, maxHeight: 40 }}
             />
           ))
         )}
